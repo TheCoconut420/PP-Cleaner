a9f4c83168c160f68d25ec0b883318ecea00febe
Stefan The Coconut <57968327+TheCoconut420@users.noreply.github.com>
2022-11-02 22:57:35 +0100

    1

---
+++ main.py
@@ -0,0 +1,17 @@
+import get_hex
+import check_extensions
+import get_all_files
+
+def main():
+    PNG = "89504E470D0A"
+    GIF = "474946383961"
+    JPG = "FFD8FFE00010"
+    path = r"C:\Users\stefa\OneDrive\Dokumente\GitHub\PP-Cleaner\Clean"
+    file_list_location = get_all_files.get_files_location(path)
+    for i in range(len(file_list_location)):
+        file = file_list_location[i]
+        hex = get_hex.get_hex(file)
+        check_extensions.check_extensions(hex, PNG, JPG, GIF, file)
+
+if __name__ == "__main__":
+    main()
